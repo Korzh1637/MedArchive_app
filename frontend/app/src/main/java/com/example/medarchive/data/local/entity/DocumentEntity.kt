579aec4869bb382ad9728fdfca38c3fe67d43d57
Korzh1637 <d.gladysheva@g.nsu.ru>
2026-04-18 00:15:43 +0700

    Update DocumentEntity.kt

--- frontend/app/src/main/java/com/example/medarchive/data/local/entity/DocumentEntity.kt
+++ frontend/app/src/main/java/com/example/medarchive/data/local/entity/DocumentEntity.kt
@@ -13,8 +13,8 @@ data class DocumentEntity(
     val title: String? = null,
     val documentType: String? = null,
     val content: String? = null,
-    val imagePath: String? = null,
-    val addImagePath: Boolean = false,
+    val imagePath: String? = null,      // ПЕРЕВЕСТИ В БИНАРНЫЙ ФОРМАТ 
+    val addImagePath: Boolean = false,  // УДАЛИТЬ
     val createdAt: Date = Date(),
     val updatedAt: Date = Date(),
     val deletedAt: Date? = null,
